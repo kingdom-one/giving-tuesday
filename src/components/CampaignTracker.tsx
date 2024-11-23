22a840497dfda2e15e3c7333573e8a9482f2b513
kjroelke <kj.roelke@gmail.com>
2024-11-22 23:37:14 -0600

    replace react component with givebutter custom element

--- src/components/CampaignTracker.tsx
+++ src/components/CampaignTracker.tsx
@@ -4,27 +4,29 @@
 import { useEffect, useState } from 'react';
 
 export default function CampaignTracker() {
-    const [campaign, setCampaign] = useState(null);
-    useEffect(() => {
-        const fetchData = async () => {
-            try {
-                const response = await fetch('/api/campaigns');
-                const data = await response.json();
-                console.log(data);
-                setCampaign(data[0]);
-            } catch (error) {
-                console.error('Error fetching data:', error);
-            }
-        };
+	const [campaign, setCampaign] = useState(null);
+	useEffect(() => {
+		const fetchData = async () => {
+			try {
+				const response = await fetch('/api/campaigns');
+				const data = await response.json();
+				console.log(data);
+				setCampaign(data[0]);
+			} catch (error) {
+				console.error('Error fetching data:', error);
+			}
+		};
 
-        fetchData();
-    }, []);
+		fetchData();
+	}, []);
 
-    return (
-		<div style={{ display: 'flex', justifyContent: 'center', flexDirection: 'column'}}>
-			 <givebutter-widget id="L0A30g"></givebutter-widget>
-			 <givebutter-widget id="pAwGOL" style={{display: 'flex', justifyContent: 'center'
-			 }}></givebutter-widget>
-		</div>
-    );
+	return (
+		<div
+			style={{
+				display: 'flex',
+				justifyContent: 'center',
+				flexDirection: 'column',
+			}}
+		></div>
+	);
 }
